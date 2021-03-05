be171ee1a50d63e33cfe5fd145cc147732a0b770
Brian Butterly <butterlybrian@gmail.com>
2021-03-05 11:10:20 -0600

    building delete function

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,7 +1,14 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import { Context } from "../context/BlogContext";
-import { EvilIcons } from '@expo/vector-icons';
+import { EvilIcons } from "@expo/vector-icons";
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
@@ -15,8 +22,12 @@ const IndexScreen = () => {
         renderItem={({ item }) => {
           return (
             <View style={styles.row}>
-              <Text style={styles.title}>{item.title}</Text>
-              <EvilIcons style={styles.icon} name="trash"/>
+              <Text style={styles.title}>
+                {item.title} - ID {item.id}
+              </Text>
+              <TouchableOpacity onPress={() => console.log(item.id)}>
+                <EvilIcons style={styles.icon} name="trash" />
+              </TouchableOpacity>
             </View>
           );
         }}
@@ -27,20 +38,20 @@ const IndexScreen = () => {
 
 const styles = StyleSheet.create({
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     paddingVertical: 20,
     paddingHorizontal: 10,
     // borderTopWidth: 1,
     borderBottomWidth: 1,
-    borderColor: 'gray'
+    borderColor: "gray",
   },
   title: {
-    fontSize: 18
+    fontSize: 18,
   },
   icon: {
-    fontSize: 24
-  }
+    fontSize: 24,
+  },
 });
 
 export default IndexScreen;
