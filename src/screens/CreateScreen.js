ddfcbfd09129fb63cc7fbf1d1dde92eb5a2886de
Brian Butterly <butterlybrian@gmail.com>
2021-03-16 15:55:08 -0500

    create blog post working

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { View, Text, TextInput, StyleSheet, Button } from "react-native";
+import { Context } from "../context/BlogContext";
 
 const CreateScreen = ({ navigation }) => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const { addBlogPost } = useContext(Context);
+
   return (
     <View>
       <Text style={styles.label}>Enter Title: </Text>
@@ -18,7 +21,10 @@ const CreateScreen = ({ navigation }) => {
         value={content}
         onChangeText={(text) => setContent(text)}
       />
-      <Button title="Add Blog Post" />
+      <Button
+        title="Add Blog Post"
+        onPress={() => addBlogPost(title, content)}
+      />
     </View>
   );
 };
